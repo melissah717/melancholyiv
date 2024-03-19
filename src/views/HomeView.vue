60e8c30be10704e2f0f56ce3f3937d6aefbc2e4f
Melissa Ho <melissa.h717@outlook.com>
2024-03-19 10:13:00 -0700

    Landing page layout finalized, dummy text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,238 +1,295 @@
-<script setup lang="ts">
-import Lenis from "@studio-freight/lenis"
+<script>
+import gsap from 'gsap';
 import { onMounted } from "vue"
+import Flower from "../components/Flower.vue" 
+import CursorFollow from "../components/CursorFollow.vue"
+import { animateElements } from "../animations"
 
-gsap.registerPlugin(ScrollTrigger)
-
-onMounted(() => {
-  const tl = gsap.timeline();
-
-  tl.to(".landing-paragraph", {
-    y: -80, // Reduced the distance for smoother transition
-    opacity: 0.7, // Starting opacity
-    scrollTrigger: {
-      trigger: ".trigger-2",
-      start: "-1000% center",
-      end: "6000% 90%",
-      pin: true,
-      pinSpacing: false,
-      scrub: true,
-    },
-  });
-
-  tl.to(".landing-banner", {
-    scrollTrigger: {
-      trigger: ".landing-banner",
-      start: "bottom 40%",
-      end: "10% 15%",
-      // markers: true,
-      pin: true,
-      pinSpacing: false,
-      scrub: 4,
-      ease: "power1.inOut"
-    },
-    y: 1100,
-  })
-
-  //   tl.to(".landing-banner", {
-  //     duration: 1.5,
-  //     y: -20,
-  //     repeat:-1,
-  //     yoyo: true
-  // });
-
-  // tl.fromTo(".star-1", {
-  //   // top: "50%", 
-  // },
-  //   {
-  //     scale: 0.65,
-  //     opacity: 1,
-  //     rotation: 90,
-  //     x: 1000,
-  //     y: -400,
-  //     top: "50%",
-  //     left: "-10%",
-  //     scrollTrigger: {
-  //       trigger: ".star-1",
-  //       start: "top top",
-  //       end: "bottom top",
-  //       scrub: true,
-  //       // markers: true
-  //     }
-  //   });
-
-  // Fade to dark mode when reaching the trigger
-  tl.to("body, .container", {
-    backgroundColor: "#000",
-    color: "#fff",
-    scrollTrigger: {
-      trigger: ".examples-container",
-      start: "35% 90%",
-      end: "center center",
-      scrub: true,
-    },
-  });
-
-  tl.to(".star-1", {
-    opacity: 1,
-    y: 500,
-    x: 500,
-    scale: 0.6,
-    scrollTrigger: {
-      trigger: ".examples-container",
-      markers: true,
-      start: "top center",
-      end: "100% bottom",
-      scrub: true
-    }
-  })
-  tl.fromTo(".orange-flower", {
-    x: -200
-  }, {
-    x: 750,
-    y: 700,
-    rotation: -50,
-    duration: 2,
-    scrollTrigger: {
-      start: "top 50%",
-      end: "10 00% 100%",
-      trigger: ".trigger-1",
-      scrub: 2,
-      // markers: true
-    }
+export default {
+  setup() {
+    onMounted(() => {
+      animateElements()
+    })
   },
-  );
-
-  tl.to(".square-1", {
-    scrollTrigger: {
-      trigger: ".landing-banner",
-      start: "top top",
-      end: "bottom 70%", // Adjusted the end value to ensure the square animation covers the entire scroll range
-      scrub: 2,
-      duration: 2,
-      // markers: true
-    },
-    y: 1500,
-    x: -400,
-    rotation: 90,
-    opacity: 0.5
-  });
-});
-const lenis = new Lenis()
-
-function raf(time: number) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-
-
+  components: {
+    Flower,
+    CursorFollow
+  },
+};
 </script>
 
 <template>
   <main>
+    <CursorFollow />
     <div class="container">
       <div class="header">
-        <div class="square-1"></div>
         <div class="landing-banner">
-          Melissa</div>
-        <div class="orange-flower">
-          <img src="../assets/orange-flower.svg" />
-        </div>
-        <div class="trigger-1"></div>
-      </div>
-      <div class="spacer"></div>
+          <h1>          Melissa</h1>
 
-      <div class="spacer"></div>
+        </div>
+      </div>
+
+      <div class="spacer">        </div>
       <div class="trigger-2"></div>
-      <!-- <span class="landing-paragraph">some optional span text here</span> -->
+      <div class="spacer"></div>
       <div class="landing-paragraph-container">
+        <!-- <Flower /> -->
         <p class="landing-paragraph">
-          This is a paragraph about melissa. I'll try to make it as short and sweet as possible because
-          she likes to stay incognito. Hopefully one day she will die in relative obscurity without spawning
-          any selfish brats to replace herself. I'm kidding though this is just some filler for some text I haven't
-          thought of yet.
-        </p>
-      </div>
-      <div class="trigger-3"></div>
-      <div class="home-container" id="home-container">
-        <div class="spacer"></div>
+          Hey there! I’m Sassy Developer Senpai, a woman who slays code like a boss. Armed with my bachelor’s in computer science, I’m the queen of algorithms and the empress of debugging. When I’m not conquering the tech world, you’ll find me strutting down the streets, capturing life’s moments through my lens. And let’s not forget my culinary prowess – I whip up code and gourmet meals with equal finesse. So, buckle up, buttercup, because this sassy developer is here to rock your world! 
 
-        <div class="right">
-          <div class="star-1"></div>
-          <div class="examples-paragraph">TRIGGER FOR DARKNESS</div>
-          <div class="trigger"></div>
+        <br />
+As a code-slinging software developer with a bachelor's in computer science, I craft digital masterpieces by day and conquer pixelated realms by night. My walks are not just a breath of fresh air—they're a quest for the perfect shot, each picture a trophy in my visual collection. In the kitchen, I'm a culinary wizard, whipping up dishes so good, they'll make your taste buds tap dance. And when it comes to gaming? I'm the undefeated champion, communicating victory and defeat with my arsenal of frog emotes. So, if you're looking for someone who's equal parts tech-savvy and life-loving, you've just hit the jackpot.
+</p>      
+<br />
+      <p class="landing-paragraph"></p>
+      </div>
+
+      <div class="spacer"></div>
+
+
+
+
+      <div class="home-container">
+      <div class="trigger-3"></div>
+      <div class="spacer"></div>
+      <div class="spacer"></div>
+      <div class="home-container" id="home-container">
+        
+        <div class="sassy">
+  <h1 class="sassy-heading">The 2000s called.</h1>
+  <div class="sassy-marquee-wrapper">
+    <marquee class="sassy-marquee">(They want their marquees back)</marquee>
+  </div>
+</div>
+
+<div class="headline-2">Your content should be as</div>
+        <div class="video-background">
+          <div class="video-foreground">
+            <iframe src="https://player.vimeo.com/video/925027042?background=1&autoplay=1&loop=1&byline=0&title=0"
+                    frameborder="0"
+                    allowfullscreen
+                    allow="autoplay; encrypted-media"
+                    title="Vimeo video">
+            </iframe>
         </div>
       </div>
-
-      <div class="star-2"></div>
-      <div class="about-container" id="about-container">
-
+      <div class="headline-3">as you are</div>
+        <div class="darkness-trigger"></div>
+        <div class="spacer"></div>
       </div>
-
-
-      <!-- <div class="examples-container" id="examples-container">
-        <h1>This is the examples view</h1>
-        <p>Right now it has a set height of 400px</p>
-      </div> -->
     </div>
+  </div>
   </main>
 </template>
 
 
+
+
 <style scoped>
-.star-1 {
-  height: 7px;
-  width: 7px;
-  background-color: rgba(255, 252, 217, 0.99);
-  -webkit-box-shadow: 0px 0px 59px 60px rgba(255, 252, 217, 0.99);
-  -moz-box-shadow: 0px 0px 59px 60px rgba(255, 252, 217, 0.99);
-  box-shadow: 0px 0px 59px 74px rgba(255, 252, 217, 0.99);
-  opacity: 0
+
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  color: rgb(10, 3, 3);
+  height: 900vh;
 }
 
-.star-2 {
-  height: 7px;
-  width: 7px;
-  z-index: 999;
-  background-color: rgba(255, 252, 217, 0.99);
-  -webkit-box-shadow: 0px 0px 59px 60px rgba(255, 252, 217, 0.99);
-  -moz-box-shadow: 0px 0px 59px 60px rgba(255, 252, 217, 0.99);
-  box-shadow: 0px 0px 59px 74px rgba(255, 252, 217, 0.99);
-  opacity: 0
-}
 
 
 .container {
   display: flex;
   flex-direction: column;
   width: 100vw;
-
-  /* background-color: rgb(56, 30, 34); */
   color: rgb(10, 3, 3);
-  height: 900vh;
+  height: auto;
 }
 
-.square-1 {
-  height: 40rem;
-  width: 80rem;
-  background-color: rgb(255, 128, 128);
-  /* border: 3px solid black; */
-  opacity: 0.1;
+.header {
+  width: 100%;
+  background-color: rgb(255, 255, 255);
+
 }
 
-.about-container,
-.home-container,
-.examples-container {
-  height: 200vh;
-  min-width: 100%;
+.landing-banner {
+  font-size: 12rem;
+  font-weight: 700;
+  letter-spacing: -2.3rem;
+  width: 100%;
+  color: black;
+  text-align: center;
+}
+
+.landing-paragraph {
+  font-size: 2em;
+  width: 50%;
+  letter-spacing: -1.5px;
+  font-weight: 700;
+  text-align: justify;
+  color: #c45b94
+}
+
+.landing-paragraph-container {
+  margin-top: 1em;
+  display: flex;
+  flex-direction: column;
+  max-width: 100vw;
+  justify-content: center;
+  align-items: center;
 }
 
 .spacer {
-  height: 45vh;
+  opacity: 0;
+  height: 100px;
+  background-color: rgb(255, 255, 255);
 }
 
+.home-container {
+  height: 150vh;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.sassy {
+  width: 80%;
+  align-items: center;
+  text-align: center;
+  font-size: 2em;
+  overflow: hidden; /* Ensures that the curtain effect is applied */
+}
+
+.sassy-heading {
+  font-size: 10rem
+}
+.sassy-heading,
+.sassy-marquee {
+  transform: translateY(100%); /* Initially move the elements out of view */
+}
+
+.sassy-marquee-wrapper {
+  overflow: hidden; /* Ensures that the marquee content is clipped */
+}
+
+.video-background {
+  position: relative;
+  width: 100%;
+  height: 50vh;
+  overflow: hidden;
+}
+
+.video-foreground {
+  position: absolute;
+
+  width: 100%;
+  height: 100%;
+}
+
+.video-foreground iframe {
+  width: 100%;
+  height: 200%;
+  /* transform: translateY(50%) */
+}
+
+
+
+.header {
+  display: flex;
+}
+.parallax-middle-picture {
+  width: 100%;
+}
+
+.parallax-top {
+  position: relative;
+}
+.parallax-top-picture {
+  width: 100%;
+  margin-top: -20rem;
+  position: absolute;
+}
+
+.parallax-lantern {
+position: absolute;
+  right: 450;
+  top: 0;
+  /* z-index: 100; */
+  width: 20%;
+}
+
+/* 
+.star-1 {
+  height: 7px;
+  width: 7px;
+  border-radius: 100%;
+  background-color: #EED098;
+  -webkit-box-shadow: 0px 0px 59px 30px #EED098;
+  -moz-box-shadow: 0px 0px 59px 30px #EED098;
+  box-shadow: 0px 0px 59px 30px #EED098;
+}
+
+.star-2 {
+  height: 70px;
+  width: 70px;
+  border-radius: 100%;
+  margin-top: 30vh;
+  border-color: #EED098;
+  background-color: #EED098;
+  -webkit-box-shadow: 0px 0px 59px 30px #EED098;
+  -moz-box-shadow: 0px 0px 59px 30px #EED098;
+  box-shadow: 0px 0px 59px 30px #EED098;
+  position: absolute;
+  right: 0;
+}
+
+.star-container,
+.big-star-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.star-sm1,
+.star-sm2,
+.star-sm3 {
+  height: 7px;
+  width: 7px;
+  border-radius: 100%;
+  background-color: #EED098;
+  -webkit-box-shadow: 0px 0px 59px 30px #EED098;
+  -moz-box-shadow: 0px 0px 59px 30px #EED098;
+  box-shadow: 0px 0px 59px 30px #EED098;
+  margin-top: 30vh;
+  /* opacity: 0 
+}
+
+.star-sm1 {
+  margin-left: 33vw;
+}
+
+.star-sm2 {
+  justify-self: center;
+  margin-left: 10vw;
+}
+
+.star-sm3 {
+  margin-left: 20vw;
+  margin-top: 20vh;
+} */
+
+/* .square-1 {
+  height: 13rem;
+  width: 18rem;
+}
+
+
+
+
+
+.parallax-middle {
+  height: 100vh
+}
 
 .trigger-1,
 .trigger-2,
@@ -242,10 +299,17 @@ requestAnimationFrame(raf)
   opacity: 0
 }
 
+.spacer-black {
+  height: 45vh;
+  background-color: black;
+  color: white;
+  margin-top: -100px;
+}
+
+
 .orange-flower {
-  height: 200px;
-  width: 200px;
-  /* position: relative; */
+  height: 100px;
+  width: 100px;
   z-index: 999;
 }
 
@@ -256,63 +320,16 @@ img {
 
 .trigger {
   height: 5vh;
-  /* background-color: pink; */
 }
 
-/*MOON*/
-
-.header {
-  background-image: "../assets/watercolor.png";
-  opacity: 0.5
-}
-
-.circle-1 {
-  height: 100px;
-  width: 100px;
-  border: 1px solid #938d9f;
-  border-radius: 100%;
-  align-self: flex-start;
-  margin: 0.1rem;
-}
-
-.circle-2 {
-  height: 135px;
-  width: 40px;
-  border: 1px solid rgba(88, 0, 0, 0.774);
-  border-radius: 100%;
-  align-self: flex-start;
-  margin-left: 3rem;
-}
-
-.star-5 {
-  height: 100px;
-  width: 100px;
-  left: 0;
-  top: 0;
-}
 
 h2 {
   font-size: 3rem;
 }
 
-.landing-banner {
-  font-size: 20rem;
-  font-weight: 700;
-  letter-spacing: -2.3rem;
-  padding-left: 1rem;
-  width: 100%;
-}
 
 .landing-paragraph {
-
-  opacity: 0.2;
   font-size: 1em;
-}
-
-.about-container {
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
 }
 
 .landing-paragraph-container {
@@ -329,4 +346,36 @@ h2 {
 span {
   font-family: "Cedarville Cursive", cursive;
 }
+
+
+.parallax-1 {
+  width: 100%;
+}
+
+.parallax-2 {
+  width: 100%;
+  z-index: 2;
+}
+
+.parallax-lantern {
+  scale: 0.5;
+  margin-top: 10%;
+  margin-left: 53%;
+  right: 0;
+}
+
+.parallax-top {
+  position: relative;
+  top: 0;
+  left: 0;
+}
+.parallax-top2 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 3;
+} */
+
 </style>
